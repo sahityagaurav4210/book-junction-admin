228a6b55914caf185e770a7b42e706db1cb08082
sahityagaurav4210 <sahityagaurav.41125@gmail.com>
2024-04-24 21:04:56 +0530

    24/4/2024 - fix: removed the logout operation on page reload

--- static/js/cp.js
+++ static/js/cp.js
@@ -1,6 +1,6 @@
-import API from "../api/index.js";
-import NotifyType from "../helpers/notification_type.helpers.js";
-import Utilities from "../utils/index.js";
+import API from '../api/index.js';
+import NotifyType from '../helpers/notification_type.helpers.js';
+import Utilities from '../utils/index.js';
 
 let dialogs = document.querySelectorAll('dialog');
 let buttons = document.querySelectorAll('button');
@@ -20,334 +20,286 @@ let deletionMsg = document.getElementById('deletionMsg');
 let unauthorizedAccessDetected = false;
 let username = new URLSearchParams(window?.location?.search).get('username');
 
-dialogs[0].style.display = "none";
-dialogs[1].style.display = "none";
+dialogs[0].style.display = 'none';
+dialogs[1].style.display = 'none';
 buttons[0].disabled = true;
-dialogs[2].style.display = "none";
-dialogs[3].style.display = "none";
-
+dialogs[2].style.display = 'none';
+dialogs[3].style.display = 'none';
 
 function isLoggedIn() {
+  if (username != null || username != undefined) {
+    let xhr = new XMLHttpRequest();
+    let formData = new FormData();
 
-    if (username != null || username != undefined) {
+    formData.append('username', username);
 
-        let xhr = new XMLHttpRequest();
-        let formData = new FormData();
+    xhr.open('POST', '/check', true);
 
-        formData.append('username', username);
-
-        xhr.open('POST', '/check', true);
-
-        xhr.onload = function () {
-
-            if (xhr.responseText == 'false') {
-
-                dialogs[3].style.display = "flex";
-                dialogs[3].showModal();
-                unauthorizedAccessDetected = true;
-
-
-                setTimeout(() => {
-                    dialogs[3].close();
-                    dialogs[3].style.display = "none";
-                    window.open('/', '_blank');
-                }, 3000);
-            }
-            else if (xhr.responseText != 'true') {
-
-                dialogs[3].style.display = "flex";
-                dialogs[3].showModal();
-                unauthorizedAccessDetected = true;
-
-
-                setTimeout(() => {
-                    dialogs[3].close();
-                    dialogs[3].style.display = "none";
-                    window.open('/', '_blank');
-                }, 3000);
-            }
-
-            if (unauthorizedAccessDetected) {
-
-                for (let i = 0; i < buttons.length; i++) {
-                    buttons[i].disabled = true;
-                }
-
-            }
-
-        }
-
-        xhr.send(formData);
-    }
-    else {
-
-        dialogs[3].style.display = "flex";
+    xhr.onload = function () {
+      if (xhr.responseText == 'false') {
+        dialogs[3].style.display = 'flex';
         dialogs[3].showModal();
         unauthorizedAccessDetected = true;
 
-        if (unauthorizedAccessDetected) {
-
-            for (let i = 0; i < buttons.length; i++) {
-                buttons[i].disabled = true;
-            }
-
-        }
+        setTimeout(() => {
+          dialogs[3].close();
+          dialogs[3].style.display = 'none';
+          window.open('/', '_blank');
+        }, 3000);
+      } else if (xhr.responseText != 'true') {
+        dialogs[3].style.display = 'flex';
+        dialogs[3].showModal();
+        unauthorizedAccessDetected = true;
 
         setTimeout(() => {
-            dialogs[3].close();
-            dialogs[3].style.display = "none";
-            window.open('/', '_blank');
+          dialogs[3].close();
+          dialogs[3].style.display = 'none';
+          window.open('/', '_blank');
         }, 3000);
+      }
+
+      if (unauthorizedAccessDetected) {
+        for (let i = 0; i < buttons.length; i++) {
+          buttons[i].disabled = true;
+        }
+      }
+    };
+
+    xhr.send(formData);
+  } else {
+    dialogs[3].style.display = 'flex';
+    dialogs[3].showModal();
+    unauthorizedAccessDetected = true;
+
+    if (unauthorizedAccessDetected) {
+      for (let i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
     }
+
+    setTimeout(() => {
+      dialogs[3].close();
+      dialogs[3].style.display = 'none';
+      window.open('/', '_blank');
+    }, 3000);
+  }
 }
 
 isLoggedIn();
 
+window.addEventListener('keydown', async function (event) {
+  if (event.ctrlKey)
+    if (event.altKey)
+      if (event.key === 'l') {
+        try {
+          const url = '/logout';
+          const payload = { username };
 
-window.addEventListener('beforeunload', async function (e) {
-    e.preventDefault();
-    e.returnValue = '';
-
-    try {
-        const url = "/logout";
-        const payload = { username };
-
-        const response = await API.makePOSTRequest(url, payload);
-        if (!response.status) {
-            Utilities.showNotification(NotifyType.WARNING, response.message);
+          await API.makePOSTRequest(url, payload);
+          window.location.href = 'http://localhost:3000';
+        } catch (error) {
+          Utilities.showNotification(NotifyType.DANGER, error.message);
         }
-    } catch (error) {
-        Utilities.showNotification(NotifyType.DANGER, error.message);
-    }
+      }
 });
 
-// wiurlndow.addEventListener('beforeunload', function (e) {
-
-//     e.preventDefault();
-//     e.returnValue = '';
-
-//     let xhr = new XMLHttpRequest();
-//     let formData = new FormData();
-
-//     formData.append('info', 'logout');
-//     formData.append('username', username);
-
-//     xhr.open('POST', '/logout', true);
-
-//     xhr.onload = function () {
-
-//         if (xhr.responseText == 'logout') {
-//             console.log('successfully logout');
-//         }
-//         else {
-//             console.log('error');
-//         }
-
-//     }
-
-//     xhr.send(formData);
-
-// });
-
 buttons[6].addEventListener('click', function () {
-    dialogs[0].style.display = "flex";
-    dialogs[0].showModal();
+  dialogs[0].style.display = 'flex';
+  dialogs[0].showModal();
 });
 
 buttons[2].addEventListener('click', function () {
-    dialogs[0].style.display = "none";
-    dialogs[0].close();
+  dialogs[0].style.display = 'none';
+  dialogs[0].close();
 });
 
 buttons[7].addEventListener('click', function () {
-    dialogs[2].style.display = "flex";
-    dialogs[2].showModal();
+  dialogs[2].style.display = 'flex';
+  dialogs[2].showModal();
 });
 
 buttons[5].addEventListener('click', function () {
-    dialogs[2].style.display = "none";
-    dialogs[2].close();
+  dialogs[2].style.display = 'none';
+  dialogs[2].close();
 });
 
 image.addEventListener('change', function () {
+  if (image.files.length == 2) {
+    let formData = new FormData();
 
-    if (image.files.length == 2) {
-        let formData = new FormData();
+    formData.append('bookPicFront', image.files[0]);
+    formData.append('bookPicRear', image.files[1]);
 
-        formData.append('bookPicFront', image.files[0]);
-        formData.append('bookPicRear', image.files[1]);
+    let xhr = new XMLHttpRequest();
 
-        let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/upload', true);
 
-        xhr.open('POST', '/upload', true);
+    xhr.onload = function () {
+      if (xhr.responseText == 'ok') {
+        message.style.display = 'flex';
+        message.innerText = 'Image Uploaded';
+        message.show();
 
-        xhr.onload = function () {
-            if (xhr.responseText == 'ok') {
-                message.style.display = "flex";
-                message.innerText = "Image Uploaded";
-                message.show();
+        image.disabled = true;
+        buttons[0].disabled = false;
 
-                image.disabled = true;
-                buttons[0].disabled = false;
+        setTimeout(() => {
+          message.innerText = '';
+          message.close();
+          message.style.display = 'none';
+        }, 2000);
+      } else if (xhr.responseText == 'file exists') {
+        message.style.display = 'flex';
+        message.innerText = 'This image is already uploaded, please change the image.';
+        message.show();
 
-                setTimeout(() => {
-                    message.innerText = "";
-                    message.close();
-                    message.style.display = "none";
-                }, 2000);
-            }
-            else if (xhr.responseText == 'file exists') {
-                message.style.display = "flex";
-                message.innerText = "This image is already uploaded, please change the image.";
-                message.show();
+        setTimeout(() => {
+          message.style.display = 'none';
+          message.innerText = '';
+          message.close();
+        }, 2000);
+      } else {
+        message.style.display = 'flex';
+        message.innerText = 'An error occured...';
+        message.show();
 
-                setTimeout(() => {
-                    message.style.display = "none";
-                    message.innerText = "";
-                    message.close();
-                }, 2000);
-            }
-            else {
-                message.style.display = "flex";
-                message.innerText = "An error occured...";
-                message.show();
+        setTimeout(() => {
+          message.style.display = 'none';
+          message.innerText = '';
+          message.close();
+        }, 2000);
+      }
+    };
 
-                setTimeout(() => {
-                    message.style.display = "none";
-                    message.innerText = "";
-                    message.close();
-                }, 2000);
-            }
-        }
-
-        xhr.send(formData);
-    }
-    else {
-        alert('Only two images are allowed..');
-    }
+    xhr.send(formData);
+  } else {
+    alert('Only two images are allowed..');
+  }
 });
 
 form[0].addEventListener('submit', function (e) {
+  e.preventDefault();
+  buttons[0].disabled = true;
 
-    e.preventDefault();
-    buttons[0].disabled = true;
+  let now = new Date();
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', '/', true);
+  xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 
-    let now = new Date();
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST', '/', true);
-    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  console.log(now);
 
-    console.log(now);
+  let formData = {
+    bookName: bookName.value,
+    bookAuthor: bookAuthor.value,
+    pubName: pubName.value,
+    pubYear: pubYear.value,
+    edition: edition.value,
+    price: price.value,
+    category: category.value,
+    phone: phone.value,
+    sold: false,
+    addedBy: username,
+    addedOn:
+      now.getDate() +
+      ' - ' +
+      (now.getMonth() + 1) +
+      ' - ' +
+      now.getFullYear() +
+      ' ' +
+      now.getHours() +
+      ' - ' +
+      now.getMinutes() +
+      ' - ' +
+      now.getSeconds(),
+  };
 
-    let formData = {
-        bookName: bookName.value,
-        bookAuthor: bookAuthor.value,
-        pubName: pubName.value,
-        pubYear: pubYear.value,
-        edition: edition.value,
-        price: price.value,
-        category: category.value,
-        phone: phone.value,
-        sold: false,
-        addedBy: username,
-        addedOn: (now.getDate() + ' - ' + (now.getMonth() + 1) + ' - ' + now.getFullYear() + ' ' + now.getHours() + ' - ' + now.getMinutes() + ' - ' + now.getSeconds())
+  message.style.display = 'flex';
+  message.innerText = "We're adding up the book details, please wait...";
+  message.show();
+
+  xhr.onload = function () {
+    buttons[0].disabled = false;
+
+    if (xhr.responseText == 'ok') {
+      image.disabled = false;
+      message.innerText = 'Book Added Successfully!!!';
+    } else if (xhr.responseText == 'Invalid data') {
+      message.innerText = 'Please provide the details in proper format...';
+    } else {
+      message.innerText = 'An error occured, please try again...';
     }
 
-    message.style.display = "flex";
-    message.innerText = "We're adding up the book details, please wait...";
-    message.show();
+    setTimeout(() => {
+      message.innerText = '';
+      message.close();
+    }, 2000);
+  };
 
-    xhr.onload = function () {
-
-        buttons[0].disabled = false;
-
-        if (xhr.responseText == 'ok') {
-            image.disabled = false;
-            message.innerText = "Book Added Successfully!!!";
-        }
-        else if (xhr.responseText == 'Invalid data') {
-            message.innerText = "Please provide the details in proper format...";
-        }
-        else {
-            message.innerText = "An error occured, please try again...";
-        }
-
-        setTimeout(() => {
-            message.innerText = "";
-            message.close();
-        }, 2000);
-    }
-
-    xhr.send(JSON.stringify(formData));
-
+  xhr.send(JSON.stringify(formData));
 });
 
-
 form[1].addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    e.preventDefault();
+  if (
+    confirm(
+      'This will delete all of your books whose name contains ' +
+        data.value +
+        ' in it. Are you sure that you want to delete all of those books?'
+    )
+  ) {
+    buttons[3].disabled = true;
 
-    if (confirm('This will delete all of your books whose name contains ' + data.value + ' in it. Are you sure that you want to delete all of those books?')) {
+    let formData = {
+      booktoDelete: data.value,
+    };
 
-        buttons[3].disabled = true;
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', '/delete', true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 
-        let formData = {
-            booktoDelete: data.value
-        }
+    deletionMsg.innerText = "We're deleting the book of yours, please wait...";
+    deletionMsg.style.display = 'flex';
 
-        let xhr = new XMLHttpRequest();
-        xhr.open('POST', '/delete', true);
-        xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onload = function () {
+      buttons[3].disabled = false;
 
-        deletionMsg.innerText = "We're deleting the book of yours, please wait...";
-        deletionMsg.style.display = "flex";
+      if (xhr.responseText == 'deleted') {
+        deletionMsg.innerText = 'Your book has been deleted...';
 
-        xhr.onload = function () {
+        setTimeout(() => {
+          deletionMsg.innerText = '';
+          deletionMsg.style.display = 'none';
+        }, 2000);
+      } else if (xhr.responseText == 'invalid data') {
+        deletionMsg.innerText = 'Please enter the data in a valid format...';
 
-            buttons[3].disabled = false;
+        setTimeout(() => {
+          deletionMsg.innerText = '';
+          deletionMsg.style.display = 'none';
+        }, 2000);
+      } else if (xhr.responseText == "book doesn't exists") {
+        deletionMsg.innerText =
+          'This book does not exists in our database, please check the spellings of your book name and try again...';
 
-            if (xhr.responseText == 'deleted') {
-                deletionMsg.innerText = "Your book has been deleted...";
+        setTimeout(() => {
+          deletionMsg.innerText = '';
+          deletionMsg.style.display = 'none';
+        }, 2000);
+      } else {
+        deletionMsg.innerText = 'An error occured...';
 
-                setTimeout(() => {
-                    deletionMsg.innerText = "";
-                    deletionMsg.style.display = "none";
-                }, 2000);
-            }
-            else if (xhr.responseText == 'invalid data') {
-                deletionMsg.innerText = "Please enter the data in a valid format...";
+        setTimeout(() => {
+          deletionMsg.innerText = '';
+          deletionMsg.style.display = 'none';
+        }, 2000);
+      }
+    };
 
-                setTimeout(() => {
-                    deletionMsg.innerText = "";
-                    deletionMsg.style.display = "none";
-                }, 2000);
-            }
-            else if (xhr.responseText == "book doesn't exists") {
-                deletionMsg.innerText = "This book does not exists in our database, please check the spellings of your book name and try again...";
-
-                setTimeout(() => {
-                    deletionMsg.innerText = "";
-                    deletionMsg.style.display = "none";
-                }, 2000);
-            }
-            else {
-                deletionMsg.innerText = "An error occured...";
-
-                setTimeout(() => {
-                    deletionMsg.innerText = "";
-                    deletionMsg.style.display = "none";
-                }, 2000);
-            }
-        }
-
-
-        xhr.send(JSON.stringify(formData));
-    }
-
+    xhr.send(JSON.stringify(formData));
+  }
 });
 
 buttons[8].addEventListener('click', function () {
-
-    window.open('/updates?username=' + username, '_blank');
-
+  window.open('/updates?username=' + username, '_blank');
 });
--- static/js/login.js
+++ static/js/login.js
@@ -1,37 +1,34 @@
-import API from "../api/index.js";
-import Utilities from "../utils/index.js";
-import NotifyType from "../helpers/notification_type.helpers.js";
+import API from '../api/index.js';
+import Utilities from '../utils/index.js';
+import NotifyType from '../helpers/notification_type.helpers.js';
 
-let messageBox = document.getElementById("dialogBox");
-let button1 = document.getElementById("button1");
-let close = document.getElementById("close");
-let username = document.getElementById("username");
-let password = document.getElementById("password");
-let heading = document.querySelector(".heading");
+let messageBox = document.getElementById('dialogBox');
+let button1 = document.getElementById('button1');
+let close = document.getElementById('close');
+let username = document.getElementById('username');
+let password = document.getElementById('password');
+let heading = document.querySelector('.heading');
 let isLoggedIn = false;
 
-messageBox.style.display = "none";
+messageBox.style.display = 'none';
 
-button1.addEventListener("click", async function () {
+button1.addEventListener('click', async function () {
   button1.disabled = true;
-  button1.innerHTML = "Please Wait...";
+  button1.innerHTML = 'Please Wait...';
 
   try {
-    if (username.value != "" && password.value != "") {
+    if (username.value != '' && password.value != '') {
       let formData = {
         username: username.value,
         password: password.value,
       };
 
-      await API.makePOSTRequest("/login", formData);
+      await API.makePOSTRequest('/login', formData);
 
       isLoggedIn = true;
     } else {
       isLoggedIn = false;
-      Utilities.showNotification(
-        NotifyType.INFO,
-        "Please fill the form correctly"
-      );
+      Utilities.showNotification(NotifyType.INFO, 'Please fill the form correctly');
     }
   } catch (error) {
     isLoggedIn = false;
@@ -39,9 +36,9 @@ button1.addEventListener("click", async function () {
   }
 
   button1.disabled = false;
-  button1.innerHTML = "Submit";
+  button1.innerHTML = 'Submit';
 
-  if (isLoggedIn){
-      window.location.href="/?request=cp&username=" + username.value;
+  if (isLoggedIn) {
+    window.location.href = '/?request=cp&username=' + username.value;
   }
 });
